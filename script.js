6230a59a2ba2530c2c1d60743e5a761eb5234164
osasblaisekhan <blaisekhan204@gmail.com>
2024-11-26 07:01:53 -0800

    practice_work

---
+++ script.js
@@ -0,0 +1,5 @@
+const myButton = document.getElementById("popup");
+myButton.addEventListener("click", function(popup){
+    const myBox = document.getElementById("osas");
+    myBox.classList.toggle("active")
+})
